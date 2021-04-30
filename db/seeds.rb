3904cb25e8468fd0a4637f41b6a2699697cd8831
tomas agnese <tagnese1985@gmail.com>
2021-04-30 00:32:21 -0300

    structure confirmed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database.."
+Project.destroy_all
+puts "Creating database.."
+
+turna = Project.new(name: "Turna", title: "Hacemos tu vida más fácil", description: "Turna is the final project of my LeWagon batch. This is the definitve web if you're in the look for your appointment management site. In this site you can log in, search for a barber shop or a veterinary in a specific neighborhood and make you're all set to make your appointment!", img: "https://http2.mlstatic.com/D_NQ_NP_800315-MLA43135787270_082020-O.jpg", href_project: "https://www.turna-domain.monster/", href_github: "https://github.com/Joacocasanova/turna", responsive: true)
+
+airbnb = Project.new(name: "Airbnb", title: "Turna Style", description: "This is the first big project of my LeWagon career. This is our attempt to make an AirBnb copycat. Here you can log in as a renter, search for a location (you can try in Palermo, Argentina for 2 guests) and make your reservation. You can also create an apartment uploading a couple of images and making it available for the other users to rent. So be my guest (or a renter, as you wish!) and have a look at the site", img: "https://http2.mlstatic.com/D_NQ_NP_800315-MLA43135787270_082020-O.jpg", href_project: "https://github.com/ropicaza/airbnb_copy", href_github: "https://github.com/ropicaza/airbnb_copy", responsive: false)
+
+books = Project.new(name: "BookAdvisor", title: "The library of libraries", description: "Did someone say web scraping? If you're looking to buy a book, this is your place. Our scraper will try go to some of the most important libraries of Argentina and try to bring the best price of the market for your search. The site may be a little slow (after all, we are scraping a lot of s.. things!) but the objective is met: to bring you the first search result of each library.", img: "https://http2.mlstatic.com/D_NQ_NP_800315-MLA43135787270_082020-O.jpg", href_project: "https://bookadvisorar.herokuapp.com/", href_github: "https://github.com/tomag1985/Book_metasearch", responsive: true)
+
+cocktail = Project.new(name: "Mr Cocktail", title: "First personal project", description: "Ok, this may not be the most impressive webpage you'll ever see, but it's my first project and you have to concede that for a starters this looks pretty ok. Here you can create your own cocktailm upload images, give reviews of the cocktails and a couple of things more. This was my first project of the LeWagon batch and still looks nice :)", img: "https://http2.mlstatic.com/D_NQ_NP_800315-MLA43135787270_082020-O.jpg", href_project: "https://mistercocktailagn.herokuapp.com/", href_github: "https://github.com/tomag1985/mister-cocktail", responsive: true)
+
+projects = Project.new(name: "Projects", title: "The place to check for my projects", description: "If you want to check for my personal projects this is where you should check.. wait a minute, where did I see this before? This must be the nexus of the world! I'm inside the description of the site where all my projects are shown. Anyway, nothing fancy to brag here just a repository of projects to check my previous works.", img: "https://http2.mlstatic.com/D_NQ_NP_800315-MLA43135787270_082020-O.jpg", href_project: "https://mistercocktailagn.herokuapp.com/", href_github: "https://github.com/tomag1985/mister-cocktail", responsive: true)
+
+
+turna.save
+airbnb.save
+books.save
+cocktail.save
+projects.save
