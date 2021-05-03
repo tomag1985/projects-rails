4b72b71329a2b87aaa9d8cb4d793e2b649f65705
tomas agnese <tagnese1985@gmail.com>
2021-05-02 23:53:29 -0300

    fixing style

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_30_023335) do
+ActiveRecord::Schema.define(version: 2021_05_03_020530) do
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2021_04_30_023335) do
     t.boolean "responsive"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "user"
+    t.string "password"
   end
 
 end
